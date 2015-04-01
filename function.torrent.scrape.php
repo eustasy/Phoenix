f9f377d69b42433d3b5c6c7678ca6420afac9fb1
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-04-01 18:57:34 +0100

    Fix scraping when no torrent.

--- function.torrent.scrape.php
+++ function.torrent.scrape.php
@@ -8,13 +8,13 @@ function torrent_scrape() {
 	require_once __DIR__.'/function.mysqli.fetch.once.php';
 
 	// select seeders and leechers
-	$query = 'SELECT '.
-		'`info_hash`,'.
-		'SUM(`state`=\'1\') AS `seeders`, '.
-		'SUM(`state`=\'0\') AS `leechers` '.
-	// from peers
-	'FROM `'.$settings['db_prefix'].'peers` ';
-	$query .= 'WHERE `info_hash`=\''.$_GET['info_hash'].'\'';
+	$query = '
+		SELECT
+			`info_hash`,
+			SUM(`state`=\'1\') AS `seeders`,
+			SUM(`state`=\'0\') AS `leechers`
+		FROM `'.$settings['db_prefix'].'peers`
+		WHERE `info_hash`=\''.$_GET['info_hash'].'\';';
 	$scrape = mysqli_fetch_once($query);
 
 	if ( !$scrape ) {
@@ -56,7 +56,25 @@ function torrent_scrape() {
 			);
 
 		} else {
-			echo 'd5:filesd20:'.$scrape['info_hash'].'d8:completei'.$scrape['seeders'].'e10:downloadedi'.$scrape['downloads'].'e10:incompletei'.$scrape['leechers'].'eeee';
+			$echo = 'd
+	5:files
+	d
+		20:'.hex2bin($_GET['info_hash']).'
+		d
+			8:complete
+			i'.$scrape['seeders'].'e
+			10:downloaded
+			i'.$scrape['downloads'].'e
+			10:incomplete
+			i'.$scrape['leechers'].'e
+		e
+	e
+e';
+			if ( isset($_GET['verbose']) ) {
+				echo $echo;
+			} else {
+				echo preg_replace('/\s+/', '', $echo);
+			}
 		}
 
 	}
