ebc954d9791c2f34630569b30f24369032649b0f
Michael Casadevall <mcasadevall@ubuntu.com>
2015-08-17 17:03:07 -0400

    Explicately delete then readd clients on update to work around stupid clients/MySQL stupidity(?)
    Signed-off-by: Michael Casadevall <mcasadevall@ubuntu.com>

--- function.peer.event.php
+++ function.peer.event.php
@@ -50,13 +50,18 @@ function peer_event() {
 		// No Existing Peer
 		!$peer ||
 		// IP has changed.
-		$peer['ip'] != $_GET['ip'] ||
+		$peer['ipv4'] != $_GET['ipv4'] ||
+		$peer['ipv6'] != $_GET['ipv6'] ||
 		// Port has changed.
-		$peer['port'] != $_GET['port'] ||
+		$peer['portv4'] != $_GET['portv4'] ||
+		$peer['portv6'] != $_GET['portv6'] ||
 		// check whether seeding status match
 		$peer['state'] != $settings['seeding']
 	) {
 		require_once __DIR__.'/function.peer.new.php';
+		require_once __DIR__.'/function.peer.delete.php';
+		// Delete and reload the peer
+		peer_delete();
 		peer_new();
 		// HOOK PEER NEW/CHANGE
 		if ( is_readable(__DIR__.'/hook.peer.change.php') ) {
