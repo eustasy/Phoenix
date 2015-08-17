03172ccb21462cbc18c1c43124b51d359e7d89c6
Michael Casadevall <mcasadevall@ubuntu.com>
2015-08-17 17:22:40 -0400

    Praising of IPv6 addresses that are in the form of rfc2732 spec
    Signed-off-by: Michael Casadevall <mcasadevall@ubuntu.com>

--- function.annouce.validate.php
+++ function.annouce.validate.php
@@ -23,10 +23,38 @@ function validate_ipv6() {
 	} else {
 		// Nope. This just got considerably more annoying.
 		// Our first char must be '['
-		// FIXME: finish this
-		if ( $client_ip[0] ==! '[' ) {
+		if ( $_GET['ipv6'][0] ==! '[' ) {
 			tracker_error('Invalid IPv6 address');
 		}
+
+		$end_mark = strpos($_GET['ipv6'], ']')-1;
+		if ( !$end_mark ) {
+			tracker_error('Invalid IPv6 address');
+		}
+
+		$v6_addr = substr($_GET['ipv6'], 1, $end_mark);
+		if (! filter_var($v6_addr, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) ) {
+			tracker_error('Invalid IPv6 address');
+		}
+
+		// Got the address, fish the port if its there
+		if ( (strlen($v6_adr)+2) == strlen($_GET['ipv6']) ) {
+			// Port wasn't included, copy it from port= if its there ...
+			if ( isset($_GET['port']) && is_numeric($_GET['port']) ) {
+				$_GET['portv6'] = $_GET['port'];
+			} else {
+				tracker_error('Invalid port specification');
+			}
+			$_GET['ipv6'] = $v6_addr;
+		}
+
+		// Try to get the port from the end of the string
+		$_GET['portv6'] = substr($_GET['ipv6'], strlen($v6_addr)+3);
+		$_GET['ipv6'] = $v6_addr;
+		if (!is_numeric($_GET['portv6'])) {
+			tracker_error('Invalid Port at end of v6 string');
+		}
+
 	}
 }
 
