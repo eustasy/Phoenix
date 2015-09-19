46e627b2841c1f80cd8c3982443848a7d6c5fbaf
Lewis Goddard <lewisgoddard@eustasy.org>
2015-09-19 07:06:51 +0100

    Update function.task.php

--- function.task.php
+++ function.task.php
@@ -1,6 +1,7 @@
 <?php
 
-function task($task, $value){
+function task($task, $value) {
+	global $connection, $settings;
 	$task = mysqli_query(
 		$connection,
 		'REPLACE INTO `'.$settings['db_prefix'].'tasks` (`name`, `value`) VALUES (\''.$task.'\', \''.$value.'\');'
