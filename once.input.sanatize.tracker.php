4a82f1c0e8141ee59a8955aeb9cebc9cb4eb1c7b
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-07 22:24:19 +0000

    Update once.input.sanatize.tracker.php

--- once.input.sanatize.tracker.php
+++ once.input.sanatize.tracker.php
@@ -5,38 +5,38 @@
 // IF 20 Character Binay, convert to 40 Character Hex
 // OR 40 Character Hex
 // THEN Sanatize
-$Sanatized['GET']['info_hash'] = false;
+$peer['info_hash'] = false;
 if ( isset($_GET['info_hash']) ) {
 	// IF info_hash BINARY
 	if ( strlen($_GET['info_hash']) == 20 ) {
-		$Sanatized['GET']['info_hash'] = bin2hex($_GET['info_hash']);
+		$peer['info_hash'] = bin2hex($_GET['info_hash']);
 	// END IF info_hash BINARY
 	// IF info_hash HEX
 	} else if ( strlen($_GET['info_hash']) == 40 ) {
-		$Sanatized['GET']['info_hash'] = $_GET['info_hash'];
+		$peer['info_hash'] = $_GET['info_hash'];
 	}
 	// END IF info_hash HEX
 }
-if ( !empty($Sanatized['GET']['info_hash']) ) {
-	$Sanatized['GET']['info_hash'] = htmlentities($Sanatized['GET']['info_hash'], ENT_QUOTES, 'UTF-8');
+if ( !empty($peer['info_hash']) ) {
+	$peer['info_hash'] = htmlentities($peer['info_hash'], ENT_QUOTES, 'UTF-8');
 }
 
 ////	$_GET['peer_id']
 // IF 20 Character Binay, convert to 40 Character Hex
 // OR 40 Character Hex
 // THEN Sanatize
-$Sanatized['GET']['peer_id'] = false;
+$peer['peer_id'] = false;
 if ( isset($_GET['peer_id']) ) {
 	// IF peer_id BINARY
 	if ( strlen($_GET['peer_id']) == 20 ) {
-		$Sanatized['GET']['peer_id'] = bin2hex($_GET['peer_id']);
+		$peer['peer_id'] = bin2hex($_GET['peer_id']);
 	// END IF peer_id BINARY
 	// IF peer_id HEX
 	} else if ( strlen($_GET['peer_id']) == 40 ) {
-		$Sanatized['GET']['peer_id'] = $_GET['peer_id'];
+		$peer['peer_id'] = $_GET['peer_id'];
 	}
 	// END IF peer_id HEX
 }
-if ( !empty($Sanatized['GET']['peer_id']) ) {
-	$Sanatized['GET']['peer_id'] = htmlentities($Sanatized['GET']['peer_id'], ENT_QUOTES, 'UTF-8');
+if ( !empty($peer['peer_id']) ) {
+	$peer['peer_id'] = htmlentities($peer['peer_id'], ENT_QUOTES, 'UTF-8');
 }
