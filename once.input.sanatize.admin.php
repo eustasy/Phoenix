0540c06854f4b69ff23824027502945715edffeb
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-07 15:12:11 +0000

    Update once.input.sanatize.admin.php

--- once.input.sanatize.admin.php
+++ once.input.sanatize.admin.php
@@ -1,5 +1,11 @@
 <?php
 
 ////	Text
-$Sanatized['POST']['process'] = htmlentities($_POST['process'], ENT_QUOTES, 'UTF-8');
-$Sanatized['GET']['message']  = htmlentities($_GET['message'],  ENT_QUOTES, 'UTF-8');
+
+// $_POST['process']
+$Sanatized['POST']['process'] = false;
+if ( !empty($_POST['process']) ) $Sanatized['POST']['process'] = htmlentities($_POST['process'], ENT_QUOTES, 'UTF-8');
+
+// $_GET['message']
+$Sanatized['GET']['message'] = false;
+if ( !empty($_GET['message']) ) $Sanatized['GET']['message'] = htmlentities($_GET['message'], ENT_QUOTES, 'UTF-8');
