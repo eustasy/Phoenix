d950a4ac08eb372565a582bbea37dc0f833fc8ad
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-13 13:42:43 +0000

    Sanatized peer_event

--- function.peer.event.php
+++ function.peer.event.php
@@ -1,15 +1,15 @@
 <?php
 
-function peer_event($connection, $settings, $time) {
+function peer_event($connection, $settings, $time, $peer) {
 
 	require_once __DIR__.'/function.mysqli.fetch.once.php';
 
-	$peer = mysqli_fetch_once(
+	$peer['old'] = mysqli_fetch_once(
 		$connection,
 		// SELECT the Peer
 		'SELECT * FROM `'.$settings['db_prefix'].'peers` '.
 		// that matches the given info_hash and peer_id
-		'WHERE `info_hash`=\''.$_GET['info_hash'].'\' AND `peer_id`=\''.$_GET['peer_id'].'\';'
+		'WHERE `info_hash`=\''.$peer['info_hash'].'\' AND `peer_id`=\''.$peer['peer_id'].'\';'
 	);
 
 	// IF Event
@@ -18,7 +18,7 @@ function peer_event($connection, $settings, $time) {
 		if ( $_GET['event'] == 'stopped' ) {
 			if ( $peer ) {
 				require_once __DIR__.'/function.peer.delete.php';
-				peer_delete($connection, $settings);
+				peer_delete($connection, $settings, $peer);
 				// HOOK PEER DELETE
 				if ( is_readable(__DIR__.'/hook.peer.delete.php') ) {
 					include __DIR__.'/hook.peer.delete.php';
@@ -34,7 +34,7 @@ function peer_event($connection, $settings, $time) {
 			$settings['seeding'] = 1;
 			// Increment downloads
 			require_once __DIR__.'/function.peer.completed.php';
-			peer_completed($connection, $settings);
+			peer_completed($connection, $settings, $peer);
 			// HOOK DOWNLOAD COMPLETE
 			if ( is_readable(__DIR__.'/hook.download.complete.php') ) {
 				include __DIR__.'/hook.download.complete.php';
@@ -46,18 +46,18 @@ function peer_event($connection, $settings, $time) {
 	// IF Any Change
 	if (
 		// No Existing Peer
-		!$peer ||
+		!$peer['old'] ||
 		// IP has changed.
-		$peer['ipv4'] != $_GET['ipv4'] ||
-		$peer['ipv6'] != $_GET['ipv6'] ||
+		$peer['ipv4'] != $peer['old']['ipv4'] ||
+		$peer['ipv6'] != $peer['old']['ipv6'] ||
 		// Port has changed.
-		$peer['portv4'] != $_GET['portv4'] ||
-		$peer['portv6'] != $_GET['portv6'] ||
+		$peer['portv4'] != $peer['old']['portv4'] ||
+		$peer['portv6'] != $peer['old']['portv6'] ||
 		// check whether seeding status match
-		$peer['state'] != $settings['seeding']
+		$settings['seeding'] != $peer['old']['state']
 	) {
 		require_once __DIR__.'/function.peer.new.php';
-		peer_new($connection, $settings, $time);
+		peer_new($connection, $settings, $time, $peer);
 		// HOOK PEER NEW/CHANGE
 		if ( is_readable(__DIR__.'/hook.peer.change.php') ) {
 			include __DIR__.'/hook.peer.change.php';
@@ -67,7 +67,7 @@ function peer_event($connection, $settings, $time) {
 	// IF Unchanged
 	} else {
 		require_once __DIR__.'/function.peer.access.php';
-		peer_access($connection, $settings, $time);
+		peer_access($connection, $settings, $time, $peer);
 		// HOOK PEER ACCESS
 		if ( is_readable(__DIR__.'/hook.peer.access.php') ) {
 			include __DIR__.'/hook.peer.access.php';
