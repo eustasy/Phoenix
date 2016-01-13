3e89519db0395b60f1a53a2c56384f6b721e184c
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-13 16:37:06 +0000

    Create function.sanatize.maybe_binary_to_hex.php

---
+++ function.sanatize.maybe_binary_to_hex.php
@@ -0,0 +1,16 @@
+<?php
+
+function maybe_binary_to_hex($binary) {
+	if (
+		strlen($binary) == 20 ||
+		strlen($binary) == 40
+	) {
+		// IF BINARY
+		if ( strlen($binary) == 20 ) {
+			$binary = bin2hex($binary);
+		// END IF BINARY
+		return htmlentities($binary, ENT_QUOTES, 'UTF-8');
+	} else {
+		return false;
+	}
+}
