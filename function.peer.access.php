406bd6500b29a77c16264cf26bd2ac1810ba7b1f
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-13 13:42:29 +0000

    Update function.peer.access.php

--- function.peer.access.php
+++ function.peer.access.php
@@ -1,11 +1,11 @@
 <?php
 
-function peer_access($connection, $settings, $time) {
+function peer_access($connection, $settings, $time, $peer) {
 	$peer_access = mysqli_query(
 		$connection,
 		'UPDATE `'.$settings['db_prefix'].'peers` '.
-		'SET `updated`=\''.$time.'\', `left`=\''.$_GET['left'].'\' '.
-		'WHERE `info_hash`=\''.$_GET['info_hash'].'\' AND `peer_id`=\''.$_GET['peer_id'].'\';'
+		'SET `updated`=\''.$time.'\', `left`=\''.$peer['left'].'\' '.
+		'WHERE `info_hash`=\''.$peer['info_hash'].'\' AND `peer_id`=\''.$peer['peer_id'].'\';'
 	);
 	if ( $peer_access ) {
 		return true;
