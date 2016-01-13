64120c4b3fee197ab20dbce18f6f4c98ebcb18f2
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-13 13:37:47 +0000

    Update function.peer.delete.php

--- function.peer.delete.php
+++ function.peer.delete.php
@@ -1,11 +1,11 @@
 <?php
 
-function peer_delete($connection, $settings) {
+function peer_delete($connection, $settings, $peer) {
 	$peer_delete = mysqli_query(
 		$connection,
 		'DELETE FROM `'.$settings['db_prefix'].'peers` '.
 		// that matches the given info_hash and peer_id
-		'WHERE info_hash=\''.$_GET['info_hash'].'\' AND peer_id=\''.$_GET['peer_id'].'\';'
+		'WHERE info_hash=\''.$peer['info_hash'].'\' AND peer_id=\''.$peer['peer_id'].'\';'
 	);
 	if ( $peer_delete ) {
 		return true;
