7ef5d9490ca9bc3b5f4b2778f9dd19c290496637
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-13 13:53:53 +0000

    Update once.sanatize.php

--- once.sanatize.php
+++ once.sanatize.php
@@ -1,41 +1,5 @@
 <?php
 
-////	function.peer.access.php
-// $_GET['left']
-// $_GET['info_hash']
-// $_GET['peer_id']
-
-////	function.peer.completed.php
-// $_GET['info_hash']
-
-////	function.peer.delete.php
-// $_GET['info_hash']
-// $_GET['peer_id']
-
-////	function.peer.event.php
-// $_GET['info_hash']
-// $_GET['peer_id']
-// $_GET['event']
-// $_GET['ipv4']
-// $_GET['ipv6']
-// $_GET['portv4']
-// $_GET['portv6']
-
-////	function.peer.new.php
-// $_GET['ipv4']
-// $_GET['ipv6']
-// $_GET['portv4']
-// $_GET['portv6']
-// $_GET['info_hash']
-// $_GET['peer_id']
-// $_GET['left']
-
-////	function.torrent.announce.php
-// $_GET['info_hash']
-// $_GET['numwant']
-// $_GET['compact']
-// $_GET['no_peer_id']
-
 ////    function.torrent.scrape.php
 // $_GET['info_hash']
 // $_GET['xml']
@@ -48,12 +12,6 @@
 ////    function.tracker.stats.php
 // $_GET['xml']
 // $_GET['json']
-
-////    once.announce.optional.php
-// $_GET['left']
-// $_GET['compact']
-// $_GET['no_peer_id']
-// $_GET['numwant']
 
 ////    scrape.php
 // $_GET['stats']
