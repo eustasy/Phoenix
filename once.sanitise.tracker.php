71b22dbe0e2af61319ae4d8c0123f5fbde05c17f
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-14 23:01:18 +0000

    Support Multiple Torrent Scraping in Sanitation

--- once.sanitise.tracker.php
+++ once.sanitise.tracker.php
@@ -3,13 +3,20 @@
 require_once __DIR__.'/function.sanitise.maybe_binary_to_hex.php';
 
 ////	$_GET['info_hash']
-// TODO: Handle Multiples
 // IF 20 Character Binay, convert to 40 Character Hex
 // OR 40 Character Hex
 // THEN Sanatize
 $peer['info_hash'] = false;
-if ( isset($_GET['info_hash']) ) {
-	$peer['info_hash'] = maybe_binary_to_hex($_GET['info_hash']);
+$peer['info_hashes'] = array();
+$params = explode('&', $_SERVER['QUERY_STRING']);
+foreach ( $params as $param ) {
+	$param = explode('=', $param, 2);
+	if ( $param[0] == 'info_hash' ) {
+		$peer['info_hashes'][] = maybe_binary_to_hex($param[1]);
+	}
+}
+if ( !empty($peer['info_hashes'][0]) ) {
+	$peer['info_hash'] = $peer['info_hashes'][0];
 }
 
 ////	$_GET['peer_id']
