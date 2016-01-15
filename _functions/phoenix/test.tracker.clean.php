9fc99a0a9fdab48088662f93e7204a9c6407a1c7
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-15 20:31:57 +0000

    Update test.tracker.clean.php

--- _functions/phoenix/test.tracker.clean.php
+++ _functions/phoenix/test.tracker.clean.php
@@ -3,3 +3,4 @@
 require_once __DIR__.'/../../_phoenix.php';
 require_once $settings['functions'].'function.tracker.clean.php';
 $result = tracker_clean($connection, $settings, $time);
+var_dump($result);
