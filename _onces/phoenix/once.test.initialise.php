0a6e0d53da8fe1eafb82deddd4cff9334523135b
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-15 19:10:06 +0000

    Create once.test.initialise.php

---
+++ _onces/phoenix/once.test.initialise.php
@@ -0,0 +1,10 @@
+<?php
+
+$test_db = mysqli_connect('127.0.0.1', 'root', '', 'phoenix');
+if ( !$test_db ) {
+	exit('Failed to connect to database for testing.');
+}
+$query = 'CREATE USER phoenix@localhost IDENTIFIED BY \'Password1\';';
+$query .= 'GRANT ALL PRIVILEGES ON *.* TO phoenix@localhost;';
+$query .= 'FLUSH PRIVILEGES;'';
+mysqli_multi_query($test_db, $query);
