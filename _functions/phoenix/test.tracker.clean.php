8f20614f91907f8ccf248aa162750ad0ea1e0c8a
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-16 16:59:45 +0000

    Update test.tracker.clean.php

--- _functions/phoenix/test.tracker.clean.php
+++ _functions/phoenix/test.tracker.clean.php
@@ -4,5 +4,6 @@ require_once __DIR__.'/../../_phoenix.php';
 require_once $settings['functions'].'function.tracker.clean.php';
 $result = tracker_clean($connection, $settings, $time);
 if ( !$result ) {
-	exit('Error #'.mysqli_errno($connection).': "'.mysqli_error($connection).'"');
+	echo 'Error: Test for Function "task" failed.';
+	exit(1);
 }
