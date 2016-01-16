bad275e93ad10b84147e069dbdb7d22cac22a280
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-16 00:24:40 +0000

    Update function.tracker.clean.php

--- _functions/phoenix/function.tracker.clean.php
+++ _functions/phoenix/function.tracker.clean.php
@@ -3,22 +3,19 @@
 function tracker_clean($connection, $settings, $time) {
 	require_once $settings['functions'].'function.task.php';
 
-	if ( mt_rand(1, 100) <= $settings['clean_idle_peers'] ) {
-		$clean = mysqli_query(
-			$connection,
-			// Delete Peers that have been idle twice the announce interval.
-			'DELETE FROM `'.$settings['db_prefix'].'peers` WHERE `updated` < '.
-			'\''. ( $time - ( $settings['announce_interval'] * 2 ) ) .'\';'
-		);
-		if ( !$clean ) {
-			tracker_error('Could not perform maintenance.');
-		}
+	$clean = mysqli_query(
+		$connection,
+		// Delete Peers that have been idle twice the announce interval.
+		'DELETE FROM `'.$settings['db_prefix'].'peers` WHERE `updated` < '.
+		'\''. ( $time - ( $settings['announce_interval'] * 2 ) ) .'\';'
+	);
+	if ( !$clean ) {
+		tracker_error('Could not perform maintenance.');
+	}
 
-		$task = task($connection, $settings, 'prune', $time);
-		if ( !$task ) {
-			tracker_error('Could not set last maintenance time.');
-		}
-
+	$task = task($connection, $settings, 'prune', $time);
+	if ( !$task ) {
+		tracker_error('Could not set last maintenance time.');
 	}
 
 	return true;
