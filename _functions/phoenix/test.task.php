9fbf24e3f3ba06d9aba7bc236ceeafc5a6c4e6fd
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-16 17:00:09 +0000

    Update test.task.php

--- _functions/phoenix/test.task.php
+++ _functions/phoenix/test.task.php
@@ -3,5 +3,6 @@ require_once __DIR__.'/../../_phoenix.php';
 require_once $settings['functions'].'function.task.php';
 $result = task($connection, $settings, $task, $value);
 if ( !$result ) {
-	exit('Error: Test for Function "task" failed.');
+	echo 'Error: Test for Function "task" failed.';
+	exit(1);
 }
