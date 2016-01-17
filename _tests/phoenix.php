087b6b3e0e5aa09b28071942ad82f76d990fe132
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-17 21:00:34 +0000

    Create phoenix.php

---
+++ _tests/phoenix.php
@@ -0,0 +1,41 @@
+<?php
+
+////	Glob Recursive Function
+// Glob Recursively to a Pattern
+function glob_recursive($Pattern, $Flags = 0, $Strip_Underscore = false) {
+	$Return = array();
+	// Search in the Current Directory
+	foreach ( glob($Pattern, $Flags) as $File) {
+		if (
+			!$Strip_Underscore ||
+			strpos($File, '/_') === false
+		) {
+			$Return[] = realpath($File);
+		}
+	}
+	// FOREACHDIRECTORY
+	// Search in ALL sub-directories.
+	foreach ( glob(dirname($Pattern).'/*', GLOB_ONLYDIR | GLOB_NOSORT) as $Directory ) {
+		// This is a recursive function.
+		// Usually, THIS IS VERY BAD.
+		// For searching recursively however,
+		// it does make some sense.
+		if (
+			!$Strip_Underscore ||
+			strpos($Directory, '/_') === false
+		) {
+			$Return = array_merge($Return, glob_recursive($Directory.'/'.basename($Pattern), $Flags, $Strip_Underscore));
+		}
+	} // FOREACHDIRECTORY
+	return $Return;
+}
+
+function require_all_once($Directory) {
+	global $Sitewide;
+	foreach (glob_recursive($Directory.'*.php') as $File) {
+		require_once $File;
+	}
+}
+
+require_once __DIR__.'/../../_phoenix.php';
+require_all_once(__DIR__.'/phoenix/');
