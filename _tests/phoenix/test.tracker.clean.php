fc946060a0f3573cbe996f26841fc8645433d9d6
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-17 21:29:46 +0000

    Update test.tracker.clean.php

--- _tests/phoenix/test.tracker.clean.php
+++ _tests/phoenix/test.tracker.clean.php
@@ -1,8 +1,10 @@
 <?php
 
+echo 'Starting Test: Tracker Clean';
 require_once $settings['functions'].'function.tracker.clean.php';
 $result = tracker_clean($connection, $settings, $time);
 if ( !$result ) {
 	echo 'Error: Test for Function "task" failed.';
 	exit(1);
 }
+echo 'Finished Test: Tracker Clean';
