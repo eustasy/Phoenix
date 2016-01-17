132f638e8dafeaeb871d69cd0f19aa6c10e9f767
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-17 21:29:28 +0000

    Update test.task.php

--- _tests/phoenix/test.task.php
+++ _tests/phoenix/test.task.php
@@ -1,8 +1,10 @@
 <?php
 
+echo 'Starting Test: Task';
 require_once $settings['functions'].'function.task.php';
 $result = task($connection, $settings, '__TASK__', 1);
 if ( !$result ) {
 	echo 'Error: Test for Function "task" failed.';
 	exit(1);
 }
+echo 'Finished Test: Task';
