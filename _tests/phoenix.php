c7621b35e1ec8bff7b8d131a66dd6c1672d899ef
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-17 21:40:00 +0000

    Update phoenix.php

--- _tests/phoenix.php
+++ _tests/phoenix.php
@@ -3,5 +3,7 @@
 require_once __DIR__.'/../_phoenix.php';
 
 foreach ( glob(__DIR__.'/phoenix/*.php') as $test) {
+	echo 'Starting Test: '.$test;
 	require_once $test;
+	echo 'Ending Test: '.$test;
 }
