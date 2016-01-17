8e8a631127298ed97e77b0f502b39e46537dae04
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-17 20:57:23 +0000

    Update test.task.php

--- _tests/phoenix/test.task.php
+++ _tests/phoenix/test.task.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/../../_phoenix.php';
+
 require_once $settings['functions'].'function.task.php';
 $result = task($connection, $settings, '__TASK__', 1);
 if ( !$result ) {
