808dc4890b75296b52ff01181ae37bdf06728320
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-18 16:46:34 +0000

    Create test.mysqli.drop.table.php

---
+++ _tests/phoenix/test.mysqli.drop.table.php
@@ -0,0 +1,13 @@
+<?php
+
+$failure = false;
+require_once $settings['functions'].'function.mysqli.drop.table.php';
+
+$create = 'CREATE TABLE `__TEST__` ( `id` int(10) );';
+$result = mysqli_query($connection, $create);
+
+$result = drop_table($connection, $settings, '__TEST__');
+if ( !$result ) {
+	echo 'Error: Empty query was not empty.'.PHP_EOL;
+	exit(1);
+}
