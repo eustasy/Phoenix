7f00024f9fc025e7622b4767e7b6e49fc9f19470
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-18 21:50:03 +0000

    Create test.mysqli.fetch.once.php

---
+++ _tests/phoenix/test.mysqli.fetch.once.php
@@ -0,0 +1,27 @@
+<?php
+
+$failure = false;
+require_once $settings['functions'].'function.mysqli.array.build.php';
+$sql = 'SELECT `info_hash` FROM `'.$settings['db_prefix'].'torrents`;';
+
+$result = mysqli_fetch_once($connection, $sql);
+if ( $result ) {
+	echo 'Error: Query returned items for an expected empty result.'.$count.PHP_EOL;
+	$failure = true;
+}
+
+$insert = 'INSERT INTO `'.$settings['db_prefix'].'torrents` ( `info_hash` ) VALUES (\'__TEST_1__\'),  (\'__TEST_2__\'),  (\'__TEST_3__\');';
+$result = mysqli_query($connection, $insert);
+
+$result = mysqli_fetch_once($connection, $sql);
+$count = count($result);
+if ( $count != 3 ) {
+	echo 'Error: Query for 3 items returned '.$count.PHP_EOL;
+	$failure = true;
+}
+
+$insert = 'DELETE FROM `'.$settings['db_prefix'].'torrents` WHERE `info_hash` LIKE \'__TEST_%\';';
+$result = mysqli_query($connection, $insert);
+if ( $failure ) {
+	exit(1);
+}
