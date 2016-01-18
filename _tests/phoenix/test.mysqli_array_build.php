ff68605aea4c8bef1e093db33ac2984c0a183c06
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-18 15:46:48 +0000

    Create test.mysqli_array_build.php

---
+++ _tests/phoenix/test.mysqli_array_build.php
@@ -0,0 +1,25 @@
+<?php
+
+$failure = false;
+require_once $settings['functions'].'function.mysqli_array_build.php';
+
+$select = 'SELECT `info_hash` FROM `'.$settings['db_prefix'].'torrents`;';
+$result = mysqli_array_build($connection, $select);
+if ( !empty($result) ) {
+	echo 'Error: Empty query was not empty.'.PHP_EOL;
+	$failure = true;
+}
+
+$insert = 'INSERT INTO `'.$settings['db_prefix'].'torrents` ( `info_hash` ) VALUES (\'__TEST_1__\'),  (\'__TEST_2__\'),  (\'__TEST_3__\');';
+$result = mysqli_query($connection, $insert);
+
+$result = mysqli_array_build($connection, $select);
+$count = count($result);
+if ( $count != 3 ) {
+	echo 'Error: Query for 3 items returned '.$count.PHP_EOL;
+	$failure = true;
+}
+
+if ( $failure ) {
+	exit(1);
+}
