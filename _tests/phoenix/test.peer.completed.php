73def0d4f3fd40b3a6471aceb678725125504174
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-19 18:34:40 +0000

    Clean up better

--- _tests/phoenix/test.peer.completed.php
+++ _tests/phoenix/test.peer.completed.php
@@ -6,6 +6,9 @@ $peer['info_hash'] = '__TEST_1__';
 
 $result = peer_completed($connection, $settings, $peer);
 
+$delete = 'DELETE FROM `'.$settings['db_prefix'].'torrents` WHERE `info_hash` LIKE \'__TEST_%\';';
+mysqli_query($connection, $delete);
+
 if ( !$result ) {
 	echo 'Function did not execute successfully.'.PHP_EOL;
 	exit(1);
--- _tests/phoenix/test.tracker.clean.php
+++ _tests/phoenix/test.tracker.clean.php
@@ -1,7 +1,14 @@
 <?php
 
 require_once $settings['functions'].'function.tracker.clean.php';
+
 $result = tracker_clean($connection, $settings, $time);
+
+////	WARNING
+// This test doesn't clean up after itself
+// as the only way to test it is to actually
+// run a clean on the tracker.
+
 if ( !$result ) {
 	echo 'Error: Test for Function "task" failed.';
 	exit(1);
