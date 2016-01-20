89ebf99b5e9e6d368ce9bcb7ac92e9395999020a
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-20 22:13:21 +0000

    Improve cleaning

--- _functions/phoenix/function.task.clean.php
+++ _functions/phoenix/function.task.clean.php
@@ -2,19 +2,34 @@
 
 function task_clean($connection, $settings, $time) {
 	require_once $settings['functions'].'function.task.log.php';
+	$cleaned = true;
 
-	$clean = mysqli_query(
-		$connection,
-		// Delete Peers that have been idle twice the announce interval.
-		'DELETE FROM `'.$settings['db_prefix'].'peers` WHERE `updated` < '.
-		'\''. ( $time - ( $settings['announce_interval'] * 3 ) ) .'\';'
-	);
-	// TODO Also clean out possible test items.
+	// Delete Peers that have been idle twice the announce interval.
+	$sql[] = 'DELETE FROM `'.$settings['db_prefix'].'peers`'.
+		' WHERE `updated` < \''. ( $time - ( $settings['announce_interval'] * 3 ) ) .'\''.
+		' OR `info_hash` LIKE \'__TEST_%\''.
+		' OR `info_hash` = \'DELETEME\''.
+		' OR `peer_id` LIKE \'__TEST_%\''.
+		' OR `peer_id` = \'DELETEME\';';
+	$sql[] = 'DELETE FROM `'.$settings['db_prefix'].'tasks`'.
+		' WHERE `name` LIKE \'__TEST_%\''.
+		' OR `name` = \'DELETEME\';';
+	$sql[] = 'DELETE FROM `'.$settings['db_prefix'].'torrents`'.
+		' WHERE `info_hash` LIKE \'__TEST_%\''.
+		' OR `info_hash` = \'DELETEME\''.
+		' OR `name` LIKE \'__TEST_%\''.
+		' OR `name` = \'DELETEME\';';
+	foreach ( $sql as $query ) {
+		$result = mysqli_query($connection, $query);
+		if ( !$result ) {
+			$cleaned = false;
+		}
+	}
 
-	if ( $clean ) {
+	if ( $cleaned ) {
 		task_log($connection, $settings, 'clean', $time);
 	}
 
-	return $clean;
+	return $cleaned;
 
 }
