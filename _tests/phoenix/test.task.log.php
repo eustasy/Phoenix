7d43f4381dd2f722d236d4d4dbeb883646f69af5
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-20 22:15:43 +0000

    Fix test name

--- _tests/phoenix/test.task.log.php
+++ _tests/phoenix/test.task.log.php
@@ -2,7 +2,7 @@
 
 require_once $settings['functions'].'function.task.log.php';
 
-$result = task_log($connection, $settings, '__TASK__', 1);
+$result = task_log($connection, $settings, '__TEST__', 1);
 
 $delete = 'DELETE FROM `'.$settings['db_prefix'].'tasks` WHERE `name` LIKE \'__TEST_%\';';
 mysqli_query($connection, $delete);
