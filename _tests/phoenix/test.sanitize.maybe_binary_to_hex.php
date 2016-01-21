0b333a88e8e1c6cecaf9b8a9c50a65a1036b1b84
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-21 22:29:43 +0000

    Improve test case

--- _tests/phoenix/test.sanitize.maybe_binary_to_hex.php
+++ _tests/phoenix/test.sanitize.maybe_binary_to_hex.php
@@ -27,3 +27,13 @@ if ( $result ) {
 	echo 'Positive result returned for test 3.'.PHP_EOL;
 	$failure = true;
 }
+
+// variables may be url encoded
+// should return string(40) "fce720af722a813a184c5550a924aaa60a8d9af1"
+$binary = '%fc%e7%20%afr%2a%81%3a%18LUP%a9%24%aa%a6%0a%8d%9a%f1';
+$result = maybe_binary_to_hex($binary);
+var_dump($result);
+if ( !$result ) {
+	echo 'Positive result returned for test 4.'.PHP_EOL;
+	$failure = true;
+}
