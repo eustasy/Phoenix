6896af904a88094223154586f1608256ccf207ab
Lewis Goddard <lewisgoddard@eustasy.org>
2016-04-14 16:04:14 +0100

    Fix Travis CI

--- _onces/phoenix/once.test.initialize.php
+++ _onces/phoenix/once.test.initialize.php
@@ -11,6 +11,18 @@ $settings['db_reset'] = true;
 // Over-ride the DB_Prefix setting.
 $settings['db_prefix'] = $settings['db_prefix'].'TESTING_';
 
+if ( getenv('CI') ) {
+	$root_db = mysqli_connect('localhost', 'root', '', $settings['db_name']);
+	if ( !$root_db ) {
+		echo 'Failed to connect to database for testing.';
+		$failure = true;
+	}
+	$query_grant = 'GRANT ALL ON `'.$settings['db_name'].'`.* TO \''.$settings['db_user'].'\'@\''.$settings['db_host'].'\' IDENTIFIED BY \''.$settings['db_pass'].'\';';
+	$query_flush = 'FLUSH PRIVILEGES;';
+	mysqli_query($root_db, $query_grant);
+	mysqli_query($root_db, $query_flush);
+}
+
 // Connect to the Database for testing.
 $test_db = mysqli_connect($settings['db_host'], $settings['db_user'], $settings['db_pass'], $settings['db_name']);
 if ( !$test_db ) {
