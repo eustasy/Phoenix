652092433cbd1e0e3a53034da6e5362b77f9874a
Lewis Goddard <lewisgoddard@eustasy.org>
2017-08-16 14:32:22 +0100

    Update _phoenix.php

--- _phoenix.php
+++ _phoenix.php
@@ -1,23 +1,5 @@
 <?php
 
-/*
- * Phoenix - A modern fork of PeerTracker, a lightweight PHP/SQL BitTorrent Tracker.
- * Copyright 2015 Phoenix Team
- *
- * Phoenix is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Phoenix is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Phoenix.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 $settings['phoenix_version'] = 'Phoenix Procedural v.3.1 2016-04-14 15:42:00Z eustasy';
 
 // If the root isn't a directory up, modify that here.
