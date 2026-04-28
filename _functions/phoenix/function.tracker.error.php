bef3e6713434cb90482ac119458a67c568abae1f
Lewis Goddard <lewis.goddard@eustasy.org>
2026-04-28 22:22:39 +0100

    BEP 3 mandates lowercase

--- _functions/phoenix/function.tracker.error.php
+++ _functions/phoenix/function.tracker.error.php
@@ -3,6 +3,6 @@
 ////	Fatal Error
 // Exits with a tracker-format error.
 function tracker_error($error) {
-	echo 'd14:Failure Reason'.strlen($error).':'.$error.'e';
+	echo 'd14:failure reason'.strlen($error).':'.$error.'e';
 	exit(2);
 }
