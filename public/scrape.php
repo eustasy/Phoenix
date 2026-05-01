2abcec14461288f9b6fdebbc90abcbdcd094515e
Lewis Goddard <lewis.goddard@eustasy.org>
2026-05-01 06:01:38 +0100

    Controller: Scrape

--- public/scrape.php
+++ public/scrape.php
@@ -1,18 +1,17 @@
 <?php
 
-// This file defines all the functions we will need.
-// Since it defines things, we need to make sure it isn't loaded twice.
+////	BitTorrent Scrape Endpoint (BEP 15) + Tracker Stats
 require_once __DIR__.'/../src/phoenix.php';
 
 require_once $settings['functions'].'function.sanitize.tracker.php';
 $peer = sanitize_tracker_params();
 
-if ( !$settings['open_tracker'] ) {
+if (!$settings['open_tracker']) {
 	require_once $settings['functions'].'function.tracker.validate.info.hashes.php';
 }
 
-// IF STATS
-if ( isset($_GET['stats']) ) {
+////	Stats Mode
+if (isset($_GET['stats'])) {
 	require_once $settings['model'].'stats.peers.php';
 	require_once $settings['model'].'stats.downloads.php';
 	require_once $settings['functions'].'function.stats.merge.php';
@@ -25,87 +24,129 @@ if ( isset($_GET['stats']) ) {
 		tracker_error('Unable to get stats.');
 	}
 
-	// XML
-	if ( isset($_GET['xml']) ) {
+	if (isset($_GET['xml'])) {
 		require_once $settings['views'].'xml.stats.php';
 		header('Content-Type: text/xml');
 		echo view_stats_xml($stats, $settings);
-
-	// JSON
-	} else if ( isset($_GET['json']) ) {
-		require_once $settings['functions'].'function.stats.render.json.php';
-		stats_render_json($stats, $settings);
-
-	// HTML
+	} elseif (isset($_GET['json'])) {
+		header('Content-Type: application/json');
+		echo json_encode(array(
+			'tracker' => array(
+				'version'   => '$Id: '.$settings['phoenix_version'].' $,',
+				'peers'     => $stats['peers'],
+				'seeders'   => $stats['seeders'],
+				'leechers'  => $stats['leechers'],
+				'torrents'  => $stats['torrents'],
+				'downloads' => $stats['downloads'],
+				'traffic'   => $stats['traffic'],
+			),
+		));
 	} else {
 		require_once $settings['views'].'html.stats.php';
 		echo view_stats_html($stats, $settings);
 	}
-// END IF STATS
+	exit;
+}
 
-// IF NOT STATS
-} else {
-	// IF SCRAPE
-	if (
-		$peer['info_hash'] &&
-		(
-			$settings['open_tracker'] ||
-			tracker_validate_info_hashes($peer['info_hashes'], $allowed_torrents)
-		)
-	) {
-		// Perform a Scrape on the torrent.
-		require_once $settings['functions'].'function.scrape.build.where.clause.php';
-		require_once $settings['functions'].'function.scrape.initialize.results.php';
-		require_once $settings['model'].'peers.scrape.php';
-		require_once $settings['model'].'torrents.scrape.php';
+////	Scrape Mode (specific torrents)
+if (
+	$peer['info_hash'] &&
+	(
+		$settings['open_tracker'] ||
+		tracker_validate_info_hashes($peer['info_hashes'], $allowed_torrents)
+	)
+) {
+	require_once $settings['functions'].'function.scrape.build.where.clause.php';
+	require_once $settings['functions'].'function.scrape.initialize.results.php';
+	require_once $settings['functions'].'function.scrape.merge.results.php';
+	require_once $settings['model'].'peers.scrape.php';
+	require_once $settings['model'].'torrents.scrape.php';
 
-		// BEP 15 allows a single scrape request to carry multiple info_hashes.
-		// Build WHERE clause and zero-initialise $scrape entries for all requested hashes
-		// so missing torrents still get a response rather than being silently omitted.
-		$where    = scrape_build_where_clause($peer['info_hashes']);
-		$scrape   = scrape_initialize_results($peer['info_hashes']);
-		$peers    = peers_scrape($connection, $settings, $where);
-		$torrents = torrents_scrape($connection, $settings, $where);
+	// BEP 15 allows multiple info_hashes per request.
+	// Pre-initialize results so missing torrents get zero counts instead of being omitted.
+	$where = scrape_build_where_clause($peer['info_hashes']);
+	$scrape = scrape_initialize_results($peer['info_hashes']);
+	$peers = peers_scrape($connection, $settings, $where);
+	$torrents = torrents_scrape($connection, $settings, $where);
 
-		if ( !$peers || !$torrents ) {
-			tracker_error('Unable to scrape for that torrent.');
+	if (!$peers || !$torrents) {
+		tracker_error('Unable to scrape for that torrent.');
+	}
+
+	$scrape = scrape_merge_results($peers, $torrents, $scrape);
+
+	// Render
+	if (isset($_GET['xml'])) {
+		require_once $settings['views'].'xml.scrape.php';
+		header('Content-Type: text/xml');
+		echo view_scrape_xml($scrape);
+	} elseif (isset($_GET['json'])) {
+		$json = array();
+		foreach ($scrape as $torrent) {
+			$json[$torrent['info_hash']] = array(
+				'info_hash' => $torrent['info_hash'],
+				'seeders'   => $torrent['seeders'],
+				'leechers'  => $torrent['leechers'],
+				'peers'     => $torrent['peers'],
+				'size'      => $torrent['size'],
+				'downloads' => $torrent['downloads'],
+				'traffic'   => $torrent['traffic'],
+			);
 		}
-
-		require_once $settings['functions'].'function.scrape.output.php';
-		scrape_output($scrape, $peers, $torrents);
-	// END IF SCRAPE
-
-	// IF FULL SCRAPE
-	} else if ( $settings['full_scrape'] ) {
-		// Scrape the full tracker.
-		require_once $settings['model'].'peers.scrape.all.php';
-		require_once $settings['model'].'torrents.scrape.all.php';
-
-		// Full scrape: no WHERE clause, returns all tracked torrents.
-		// $scrape is not pre-initialised here; scrape_output builds it from the results.
-		$peers    = peers_scrape_all($connection, $settings);
-		$torrents = torrents_scrape_all($connection, $settings);
-
-		if ( !$peers || !$torrents ) {
-			tracker_error('Unable to scrape for that torrent.');
-		}
-
-		require_once $settings['functions'].'function.scrape.output.php';
-		scrape_output(array(), $peers, $torrents);
-	// END IF FULL SCRAPE
-
-	// IF NOT ALLOWED TO SCRAPE
+		header('Content-Type: application/json');
+		echo json_encode($json);
 	} else {
-		// IF ERROR TORRENT
-		if ( isset($peer['info_hash']) ) {
-			tracker_error('Torrent is not allowed.');
-		// END IF ERROR TORRENT
+		require_once $settings['views'].'bencode.scrape.php';
+		echo view_scrape_bencode($scrape);
+	}
+	exit;
+}
 
-		// IF ERROR TRACKER
-		} else {
-			tracker_error('Tracker scraping is not allowed.');
-		} // END IF ERROR TRACKER
+////	Full Scrape Mode
+if ($settings['full_scrape']) {
+	require_once $settings['functions'].'function.scrape.merge.results.php';
+	require_once $settings['model'].'peers.scrape.all.php';
+	require_once $settings['model'].'torrents.scrape.all.php';
 
-	} // END IF NOT ALLOWED TO SCRAPE
+	$peers = peers_scrape_all($connection, $settings);
+	$torrents = torrents_scrape_all($connection, $settings);
 
-} // END IF NOT STATS
+	if (!$peers || !$torrents) {
+		tracker_error('Unable to scrape for that torrent.');
+	}
+
+	$scrape = scrape_merge_results($peers, $torrents);
+
+	// Render
+	if (isset($_GET['xml'])) {
+		require_once $settings['views'].'xml.scrape.php';
+		header('Content-Type: text/xml');
+		echo view_scrape_xml($scrape);
+	} elseif (isset($_GET['json'])) {
+		$json = array();
+		foreach ($scrape as $torrent) {
+			$json[$torrent['info_hash']] = array(
+				'info_hash' => $torrent['info_hash'],
+				'seeders'   => $torrent['seeders'],
+				'leechers'  => $torrent['leechers'],
+				'peers'     => $torrent['peers'],
+				'size'      => $torrent['size'],
+				'downloads' => $torrent['downloads'],
+				'traffic'   => $torrent['traffic'],
+			);
+		}
+		header('Content-Type: application/json');
+		echo json_encode($json);
+	} else {
+		require_once $settings['views'].'bencode.scrape.php';
+		echo view_scrape_bencode($scrape);
+	}
+	exit;
+}
+
+////	Not Allowed
+if (isset($peer['info_hash'])) {
+	tracker_error('Torrent is not allowed.');
+} else {
+	tracker_error('Tracker scraping is not allowed.');
+}
