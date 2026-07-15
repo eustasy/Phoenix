2df76368f4e734f3a3e24372b85dbe37d9ca104d
Lewis Goddard <lewis.goddard@eustasy.org>
2026-07-15 22:02:45 +0100

    v4.3beta9

--- tests/phoenix/ViewLoginHtmlTest.php
+++ tests/phoenix/ViewLoginHtmlTest.php
@@ -43,8 +43,8 @@ class ViewLoginHtmlTest extends TestCase
 
     public function testVersionFlowsIntoFooter(): void
     {
-        $html = view_login_html(false, false, 'v4.3beta8');
-        $this->assertStringContainsString('v4.3beta8', $html);
+        $html = view_login_html(false, false, 'v4.3beta9');
+        $this->assertStringContainsString('v4.3beta9', $html);
     }
 
     public function testReturnsString(): void
